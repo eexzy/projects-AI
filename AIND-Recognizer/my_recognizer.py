bec295af9f112e97d441a19a207c0f4f3a69ca7b
eexzy <27951093+eexzy@users.noreply.github.com>
2018-06-21 23:58:27 +0800

    Udacity

---
+++ AIND-Recognizer/my_recognizer.py
@@ -0,0 +1,42 @@
+import warnings
+from asl_data import SinglesData
+
+
+def recognize(models: dict, test_set: SinglesData):
+    """ Recognize test word sequences from word models set
+
+   :param models: dict of trained models
+       {'SOMEWORD': GaussianHMM model object, 'SOMEOTHERWORD': GaussianHMM model object, ...}
+   :param test_set: SinglesData object
+   :return: (list, list)  as probabilities, guesses
+       both lists are ordered by the test set word_id
+       probabilities is a list of dictionaries where each key a word and value is Log Liklihood
+           [{SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
+            {SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
+            ]
+       guesses is a list of the best guess words ordered by the test set word_id
+           ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
+   """
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    probabilities = []
+    guesses = []
+    # TODO implement the recognizer
+    for i in range(test_set.num_items):
+        prob = {}
+        best_word = None
+        best_score = float('-inf')
+        X, lengths = test_set.get_item_Xlengths(i)
+        for word, model in models.items():
+            try:
+                score = model.score(X, lengths)
+                prob[word] = score
+
+                if score > best_score:
+                    best_word = word
+                    best_score = score
+            except:
+                prob[word] = float('-inf')
+        probabilities.append(prob)
+        guesses.append(best_word)
+
+    return probabilities, guesses
